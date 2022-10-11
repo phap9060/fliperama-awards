fb58af9a24e4511f566d6e39499d5df139ae256f
pedro.pereira <pedro.pereira@rarolabs.com.br>
2022-10-10 22:38:09 -0300

    feat: change back the auth Domain

--- src/fireBase/config.ts
+++ src/fireBase/config.ts
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "https://fliperama-awards.vercel.app/",
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING,
