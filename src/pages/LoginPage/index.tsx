1962e57e9a9c7b2e7da28202b4fb0ac0c7536b26
pedro.pereira <pedro.pereira@rarolabs.com.br>
2022-10-10 23:20:25 -0300

    feat: just back the token in local storage

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -11,7 +11,7 @@ export const LoginPage = () => {
   const Login = async () => {
     const result = await signInWithGoogle();
     if (result) {
-      //localStorage.setItem("token", result.user.refreshToken);
+      localStorage.setItem("token", result.user.refreshToken);
       navigate(`vote`);
     }
   };
